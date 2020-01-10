9b7ec32e74932adf61e3589f9ca58a00b610a33b
ravi7299 <43462462+ravi7299@users.noreply.github.com>
2020-01-10 17:35:39 +0530

    Initial commit

---
+++ loadMovieList.m
@@ -0,0 +1,19 @@
+function movieList = loadMovieList()
+
+fid = fopen('movie_ids.txt');
+
+% Store all movies in cell array movie{}
+n = 1682;  % Total number of movies 
+
+movieList = cell(n, 1);
+for i = 1:n
+    % Read line
+    line = fgets(fid);
+    % Word Index (can ignore since it will be = i)
+    [idx, movieName] = strtok(line, ' ');
+    % Actual Word
+    movieList{i} = strtrim(movieName);
+end
+fclose(fid);
+
+end
